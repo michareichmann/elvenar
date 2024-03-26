3ea32094b5e3abadc9c364d081552abae27c7648
michareichmann <micha.reichmann@gmail.com>
2024-03-26 11:14:46 +0100

    add box2pos()

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,12 @@
 import telegram_send
 import asyncio
+from pyscreeze import Box
+from typing import Tuple
 
 
 def send_notification(msg: str):
     asyncio.run(telegram_send.send(messages=[msg]))
+
+
+def box2pos(box: Box, xoff=.5, yoff=.5) -> Tuple[int, int]:
+    return round(box.left + box.width * xoff), round(box.top + box.height * yoff)
