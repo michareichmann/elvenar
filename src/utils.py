1c232c5291087edd0d5160ac269dbfd3303a7f3a
michareichmann <micha.reichmann@gmail.com>
2024-03-26 15:41:38 +0100

    add t2ts()

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import telegram_send
 import asyncio
 from typing import Tuple
+from time import time
+import numpy as np
 
 
 def send_notification(msg: str):
@@ -9,3 +11,10 @@ def send_notification(msg: str):
 
 def box2pos(box, xoff=.5, yoff=.5) -> Tuple[int, int]:
     return round(box.left + box.width * xoff), round(box.top + box.height * yoff)
+
+
+def t2ts(t: str) -> float:
+    if 'Min' in t:
+        return time() + np.sum(np.array(t.strip('Min').split(':')).astype('i') * [60, 1])
+    elif 'Std' in t:
+        return time() + np.sum(np.array(t.strip('Std').split(':')).astype('i') * [60 * 60, 60])
