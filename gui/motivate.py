198a864b0bec9fcc8f5e35205909a0585ca96354
michareichmann <micha.reichmann@gmail.com>
2024-03-26 15:13:56 +0100

    add notification when motivation is ready

--- gui/motivate.py
+++ gui/motivate.py
@@ -6,12 +6,14 @@ from gui.group_box import GroupBox
 from gui.utils import button
 from src.elvenar import Elvenar, time
 from datetime import timedelta
+from src.utils import send_notification
 
 
 class HelpBox(GroupBox):
 
     Title = 'Motivation'
     Height = 22
+    TDay = 24 * 60 * 60
 
     def __init__(self):
         super().__init__()
@@ -19,10 +21,15 @@ class HelpBox(GroupBox):
         self.create_widgets()
         self.PBar = self.create_progress_bar()
 
+        self.Notified = False
+
     def update(self):
         elapsed_time = time() - Elvenar.LastMotivate
-        self.PBar.setFormat(str(timedelta(seconds=round(24 * 60 * 60 - elapsed_time))))
-        self.PBar.setValue(round(elapsed_time / (24 * 60 * 60) * 100))
+        self.PBar.setFormat(str(timedelta(seconds=round(self.TDay - elapsed_time))))
+        self.PBar.setValue(round(elapsed_time / self.TDay * 100))
+        if elapsed_time > self.TDay and not self.Notified:
+            send_notification('ready to motivate')
+            self.Notified = True
 
     def create_layout(self) -> QGridLayout:
         self.setLayout(QGridLayout(self))
@@ -34,6 +41,10 @@ class HelpBox(GroupBox):
         self.Layout.addWidget(pbar, 1, 0, 1, 2)
         return pbar
 
+    def help_all(self):
+        Elvenar.motivate(all_=True)
+        self.Notified = False
+
     def create_widgets(self):
         self.Layout.addWidget(button('help', partial(Elvenar.motivate, all_=False)), 0, 0)
-        self.Layout.addWidget(button('help all', partial(Elvenar.motivate, all_=True)), 0, 1)
+        self.Layout.addWidget(button('help all', self.help_all), 0, 1)
